74b0510428aec24c83531228a3bda43d9a4d235a
Michel Kenneth Hansen <michelhansen@disruptx.dk>
2013-07-20 13:31:59 +0200

    Finish user edit, update, idex and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,11 +21,13 @@ describe User do
   subject { @user }
 
   it { should respond_to(:name) }
-  it { should respond_to(:email) }
+  it { should respond_to(:email) }  
   it { should respond_to(:password_digest) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  it { should_not be_admin }
 
   it { should be_valid }
 
@@ -124,6 +126,15 @@ end
       @user.save
       @user.reload.email.should == mixed_case_email.downcase
     end
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
   end
 
 end
