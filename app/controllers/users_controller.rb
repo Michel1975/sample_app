a69ddfedb6c39489a8ddbfa729c412ebae624c4a
Michel Kenneth Hansen <michelhansen@disruptx.dk>
2013-07-24 16:59:13 +0200

    fixed issue with user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController 
-  before_filter :correct_user,   only: [:edit, :update]
-  before_filter :admin_user,     only: :destroy
   before_filter :signed_in_user,
                 only: [:index, :edit, :update, :destroy, :following, :followers]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+  
   
   def index
     @users = User.paginate(page: params[:page])
@@ -64,11 +65,11 @@ class UsersController < ApplicationController
   private
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
